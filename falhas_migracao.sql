3d6a4f62b66ec4efa9a04423ab4353c6fe01db26
Edson Magno <89936874+magnoxy@users.noreply.github.com>
2025-06-30 19:31:38 -0400

    .

--- falhas_migracao.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO TMI.NSF_MI_MI_NOTE(MI_ID, SEQUENCE, NOTE) VALUES (20644, 2, 'Se por qualquer motivo as operações definidas para o posto não puderem ser concluídas, o suporte de linha (Universal, Liderança, Engenharia ou Qualidade) deve ser acionado.');
-
-INSERT INTO TMI.NSF_MI_MI_NOTE(MI_ID, SEQUENCE, NOTE) VALUES (20644, 1, 'É de responsabilidade do operador do posto informar qualquer anomalia de processo (situação fora do normal) durante a realização das tarefas estipuladas para seu posto de trabalho.');
-
-INSERT INTO TMI.NSF_MI_MI_NOTE(MI_ID, SEQUENCE, NOTE) VALUES (20644, 3, 'O suporte de linha deve analisar ou encaminhar para análise a situação observada e se houver a necessidade de parar a linha seguir o procedimento BR_PRQA072 - Parada de linha – SMT - Guideline. - - Para maiores informações por favor checar o procedimento BR_PRQA07 - Tratamento de não conformidades.');
-
-INSERT INTO TMI.NSF_MI_MI_NOTE(MI_ID, SEQUENCE, NOTE) VALUES (20644, 4, 'As fotos e os vídeos são ilustrativos apenas para orientação do operador que deve seguir o que esta descrito como operação a ser feita.');
-
-INSERT INTO TMI.NSF_MI_MI_NOTE(MI_ID, SEQUENCE, NOTE) VALUES (20644, 5, 'Nenhuma operação deve ser feita se permanecer dúvidas, neste caso chamar suporte para esclarecimentos.');
